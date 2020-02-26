abbde72fe43d668067520d12b2a76aae54ac3298
Vaibhav Singh <vaibhavsingh100070@gmail.com>
2020-02-26 21:03:58 +0530

    Finished

--- app/src/main/java/com/example/hikerwatch/MainActivity.java
+++ app/src/main/java/com/example/hikerwatch/MainActivity.java
@@ -8,14 +8,22 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView latitude, longitude, accuracy, altitude, address;
 
     LocationManager locationManager;
 
@@ -27,17 +35,66 @@ public class MainActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
-        if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-            if(ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+            startListening();
 
-                locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-            }
         }
     }
 
-    public void updateLocationInfo(Location location){
+    public void startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+        }
+    }
+
+    public void updateLocationInfo(Location location) {
         Log.d(TAG, "updateLocationInfo: updated location " + location.toString());
+
+        latitude.setText("Latitude: " + location.getLatitude());
+        longitude.setText("Longitude: " + location.getLongitude());
+        accuracy.setText("Accuracy: " + location.getAccuracy());
+        altitude.setText("Altitude: " + location.getAltitude());
+
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+
+        try {
+            String address = "Could not find address";
+            List<Address> listAddress = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+
+            if (listAddress != null && listAddress.size() > 0) {
+
+                Log.d(TAG, "updateLocationInfo: " + listAddress.get(0).toString());
+
+                address = "";
+                if (listAddress.get(0).getSubThoroughfare() != null) {
+                    address += listAddress.get(0).getSubThoroughfare() + " ";
+                }
+
+                if (listAddress.get(0).getThoroughfare() != null) {
+                    address += listAddress.get(0).getThoroughfare() + "\n";
+                }
+
+                if (listAddress.get(0).getLocality() != null) {
+                    address += listAddress.get(0).getLocality() + "\n";
+                }
+
+                if (listAddress.get(0).getPostalCode() != null) {
+                    address += listAddress.get(0).getPostalCode() + "\n";
+                }
+
+                if (listAddress.get(0).getCountryName() != null) {
+                    address += listAddress.get(0).getCountryName();
+                }
+
+                this.address.setText("Address: "+address);
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "updateLocationInfo: Exception " + e.getMessage());
+
+            this.address.setText("Address: Couldnot find address");
+        }
 
     }
 
@@ -47,13 +104,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        latitude = findViewById(R.id.latitude);
+        longitude = findViewById(R.id.longitude);
+        accuracy = findViewById(R.id.accuracy);
+        altitude = findViewById(R.id.altitude);
+        address = findViewById(R.id.address);
+
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
 
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
 
-                Log.d(TAG, "onLocationChanged: " + location.toString());
+                updateLocationInfo(location);
 
             }
 
@@ -74,23 +137,51 @@ public class MainActivity extends AppCompatActivity {
         };
 
 
-        if(ContextCompat.checkSelfPermission(this , Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this , new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
-        }
-        else{
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER , 0 , 0 , locationListener);
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            Log.d(TAG, "onCreate: Permission not granted");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+        } else {
+            Log.d(TAG, "onCreate: Permission granted");
 
-            Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, locationListener);
 
-//            Log.d(TAG, "onCreate: location " + location.toString());
+            //  Fetch last known location
+            Location location = getLastKnownLocation();
+            Log.d(TAG, "onCreate: Last location " + location);
 
-            if(location != null){
+            if (location != null) {
                 updateLocationInfo(location);
-
             }
 
 
         }
 
+    }
+
+    private Location getLastKnownLocation() {
+//        loca = (LocationManager)getApplicationContext().getSystemService(LOCATION_SERVICE);
+        List<String> providers = locationManager.getProviders(true);
+        Location bestLocation = null;
+        for (String provider : providers) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+            }
+            Location l = locationManager.getLastKnownLocation(provider);
+            if (l == null) {
+                continue;
+            }
+            if (bestLocation == null || l.getAccuracy() < bestLocation.getAccuracy()) {
+                // Found best last known location: %s", l);
+                bestLocation = l;
+            }
+        }
+        return bestLocation;
     }
 }
